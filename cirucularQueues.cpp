8ca368b687d13e13815fa8caba6fbe3f4c23a4a4
albukhari21 <albukhariinsankamil@gmail.com>
2025-06-13 08:27:03 +0700

    Fix remove method in Queues class to reset pointers when last element is deleted

--- cirucularQueues.cpp
+++ cirucularQueues.cpp
@@ -49,6 +49,11 @@ public:
         }
         cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << endl;
 
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;
+        }
         
        
     }
