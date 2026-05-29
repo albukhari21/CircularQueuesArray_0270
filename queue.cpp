d470a9788a8ba8f8880bd3139d01751697bc1a36
albukhari21 <albukhariinsankamil@gmail.com>
2026-05-29 14:55:47 +0700

    add overflow condition check in insert function

--- queue.cpp
+++ queue.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 using namespace std;
 
-class Queues {
+class Queues 
+{
 private:
     static const int max = 3;
     int FRONT, REAR;
     int queue_array[max];
 
 public:
-    Queues() {
+    Queues() 
+    {
         FRONT = -1;
         REAR = -1;
     }
+
+    void insert() 
+    {
+        int num;
+        cout << "\nEnter a number: ";
+        cin >> num;
+        cout << endl;
+
+        // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) 
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
 };
